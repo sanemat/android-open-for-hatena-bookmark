1aab58cc5fff3edb57a9076962e6b2f0444d9cbc
Matt (Sanemat) <o.gata.ken@gmail.com>
2018-12-31 21:27:43 +0700

    chore(app): add hatebu activity

---
+++ app/src/main/java/jp/sane/openforhatenabookmark/HatebuActivity.kt
@@ -0,0 +1,12 @@
+package jp.sane.openforhatenabookmark
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class HatebuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hatebu)
+    }
+}
