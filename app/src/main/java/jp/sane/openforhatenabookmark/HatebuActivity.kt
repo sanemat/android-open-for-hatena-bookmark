798ad45ca879b1652f21105e3352d11aff5a948f
Matt (Sanemat) <o.gata.ken@gmail.com>
2018-12-31 23:54:57 +0700

    chore(app): retrive data from intent

--- app/src/main/java/jp/sane/openforhatenabookmark/HatebuActivity.kt
+++ app/src/main/java/jp/sane/openforhatenabookmark/HatebuActivity.kt
@@ -1,5 +1,6 @@
 package jp.sane.openforhatenabookmark
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,9 @@ class HatebuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hatebu)
+        if (!Intent.ACTION_VIEW.equals(intent.action)) {
+            return
+        }
+        val url = intent.data ?: return
     }
 }
