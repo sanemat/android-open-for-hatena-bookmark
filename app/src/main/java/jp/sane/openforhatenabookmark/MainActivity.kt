cd86045a62826de7e159d2fb4083a44080bc8916
Matt (Sanemat) <o.gata.ken@gmail.com>
2019-01-02 00:39:17 +0700

    chore(app): use androidx

--- app/src/main/java/jp/sane/openforhatenabookmark/MainActivity.kt
+++ app/src/main/java/jp/sane/openforhatenabookmark/MainActivity.kt
@@ -1,6 +1,6 @@
 package jp.sane.openforhatenabookmark
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
